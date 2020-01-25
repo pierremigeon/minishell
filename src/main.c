67d2e979979cc99e7fbd6f4edfa78e596b91e8eb
Pierre-Migeon <p.migeon@gmail.com>
2020-01-25 15:31:08 -0800

    checks for path and forks + executes

--- src/main.c
+++ src/main.c
@@ -155,75 +155,6 @@ int	get_key(char *str)
 	return (ft_strlen(str) % HASH_SIZE);
 }
 
-int	env(t_hlist **env_h)
-{
-	int 	n;
-	t_hlist *temp;
-
-	n = 0;
-	while (n < HASH_SIZE)
-	{
-		temp = env_h[n];
-		while (temp)
-		{
-			ft_putstr(temp->var_name);
-			ft_putchar('=');
-			ft_putendl(temp->contents);
-			temp = temp->next;
-		}
-		++n;
-	}
-	return (1);
-}
-
-int	_setenv(char *str, t_hlist **env_h)
-{
-	t_hlist *temp;
-
-	str += 6;
-	while (*str == ' ')
-		str++;
-	if (*str == '\0')
-		env(env_h);	
-	return (1);
-}
-
-void	free_thlist(t_hlist *node)
-{
-	free(node->var_name);
-	free(node->contents);
-	free(node);
-}
-
-int	_unsetenv(char *str, t_hlist ***env_h)
-{
-	t_hlist *temp;
-	t_hlist *last;
-	char	**args;
-	int	i;
-
-	args = ft_strsplit(str, ' ');
-	i = 1;
-	while (args[i])
-	{
-		temp = *env_h[get_key(args[i])];
-		last = temp;
-		while (temp && ft_strcmp(temp->var_name, args[i]) && (last = temp))		
-			temp = temp->next;
-		if (temp)
-		{
-			if (last != temp)
-				last->next = temp->next;
-			else
-				*env_h[get_key(*args)] = temp->next;
-			free_thlist(temp);
-		}
-		i++;
-	}
-	free_args(args);
-	return (1);
-}
-
 int	equal_wspace(char *str1, char *str2, size_t len)
 {
 	while (*str1 == ' ')
@@ -246,7 +177,7 @@ int	built_in(char *str, t_hlist **env_h)
 	if (equal_wspace(str, "unsetenv ", 8))
 		return (0);
 	if (equal_wspace(str, "env ", 3))
-		return (0);
+		return (1);
 	if (equal_wspace(str, "exit ", 4) || equal_wspace(str, "quit ", 4))
 		exit(0);
 	return (0);
