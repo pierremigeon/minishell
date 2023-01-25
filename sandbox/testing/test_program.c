4047bd982925a0d50591044980acd49e6e8474b6
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-01-25 13:48:06 -0800

    include header file changes

--- sandbox/testing/test_program.c
+++ sandbox/testing/test_program.c
@@ -1,12 +1,21 @@
 
-#include "./includes/minishell.h"
+// #include "./includes/minishell.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int	main()
 {
 	int i;
 	char *buf;
+
+	buf = (char *)malloc(sizeof(char) * 5);
+	buf = NULL;
+	free(buf);
+
+
+//	if (!(free(buf) & 0))
+//		printf("You freed it\n");
 
 	printf("Testing, result is %i\n", chdir(".. "));
 	printf("The cwd is %s\n", getwd(buf));
