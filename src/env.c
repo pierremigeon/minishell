3744763a0fea14cc6c8ca96e9a946a3cb710490c
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-01-31 01:11:19 -0800

    Establish basic functionality for two argument setenv with quotes and backslashes in name handled

--- src/env.c
+++ src/env.c
@@ -70,12 +70,17 @@ void	new_point(t_hlist **env_h, int key, char *str1, char *str2)
 		set_list_end(env_h[key], env_h);
 }
 
+int	isQorS(char c)
+{
+	return (c == '\"' || c == '\'' || c == '\\');
+}
+
 int	check_legal_chars(char *str)
 {
 	while (*str)
 	{
 		if (!ft_isalnum(*str) && *str != '.' && *str != '_' \
-			&& *str != '\'' && *str != '\"')
+			&& *str != '\'' && *str != '\"' && *str != '\\')
 			return(1);
 		++str;
 	}
@@ -87,69 +92,51 @@ int	esc(int flag)
 	return (flag % 2);
 }
 
-int	check_behind(char *str, int len, int *flag, int total)
+int	check_behind(char c, int *flag)
 {
-	char cur_char;
-
-	cur_char = str[len];
-	if (cur_char != '\"' && cur_char != '\'' && cur_char != '\\')
-		return (1);
-	if (*flag == 1)
-		return (0);
-	if (len == 0 || str[len - 1] != '\\')
+	printf("check_behind: %c\n", c);
+	if (*flag < 2)
 		*flag = 2;
-	if (esc(*flag))
-		return ((cur_char == '\\') ? (*flag += 1) : 1);
-	if (cur_char == '\\')
-		*flag += 1;
-	if (cur_char == str[total] && !(str[total] = '\0'))
-		return (0);
-	if (str[total] && (cur_char == '\"' || cur_char == '\''))
-		return (1);
-	else if ((cur_char == '\"' || cur_char == '\''))
-		str[total] = cur_char;
-	return (1);
+	if (c == '\\' && (*flag += 1))
+		return (esc(*flag - 1));
+	if ((c == '\"' || c == '\'') && !esc(*flag))
+		return (!(*flag = 2));
+	return ((*flag = 2));
 }
 
-int	start_quote(char *str, int i, int *b_flag, int *q_flag)
+int	q_check(char *str, char c, int total)
 {
-
-	return (1);
-
-/*
-	if (*b_flag == 0 || *b_flag > 1)
+	printf("q_check: %c\n", c);
+	if (c == str[total] && !(str[total] = '\0'))
+		return (0);
+	if (str[total] && (c == '\"' || c == '\''))
 		return (1);
-	if (str[i] == '\"' || str[i] == '\'')
-	{
-		if (i == 0)
-			*q_flag = -1;
-		else if (*q_flag < 0)
-			--*q_flag;
-	}
-	else if ()
-		
-	if 
-
-	return (1);
-*/
+	if (c == '\"' || c == '\'')
+		str[total] = c;
+	return (0);
 }
 
 char	*prepare_out(char *str, int *b_flag, int count)
 {
 	int i;
 	int x;
-	int *q_flag;
 	char *out;
 
-	i = -1,
+	i = -1;
 	x = 0;
-	if (!(out = (char *)malloc(sizeof(char) * --count)))
+	printf("Count is %i\n", count);
+	if (!(out = (char *)malloc(sizeof(char) * count--)))
 		exit(1);
+	out[count] = '\0';
 	while (str[++i])
-		if (check_behind(str, i, b_flag, count))
-			if (start_quote(str, i, q_flag, b_flag))
-				out[x++] = str[i];
-	b_flag = q_flag;
+	{
+		if (check_behind(str[i], b_flag) || q_check(out, str[i], count))
+		{
+			out[x++] = str[i];
+			printf("out[x] is %c\n", str[i]);
+		}
+	}
+	printf("out is %s and x is %i and count is %i\n", out, x, count);
 	out[x] = '\0';
 	return (out);
 }
@@ -171,16 +158,19 @@ char	*trim_qs(int count, char *str, int flag)
 	if (count < 0 && quotation_error(count))
 		return (NULL);
 	out = prepare_out(str, &flag, count);
-	if (flag < 0 && start_error(&str, 0))
-		f_out(&out);
+	printf("HEre\n");
+//	if (flag < 0 && start_error(&str, 0))
+//		f_out(&out);
 	free(str);
 	return (out);
 }
 
-void	set_counts(char c, int *c1, int *c2, int *c3)
+void	set_counts(char c, int *c1, int *c2)
 {
 	int ca[2] = { -1, -2 };
 
+	if (c == '\\')
+		return;
 	if (*c1 == 0 && (*c2 += 1))
 		*c1 = ca[(int)c % 2];
 	else if (*c1 == ca[(int)c % 2] && ((*c2 -= 1) || 1))
@@ -192,19 +182,8 @@ void	set_counts(char c, int *c1, int *c2, int *c3)
 int	pross_bksl(char c, int *i)
 {
 	if (c == '\\' && (*i += 1))
-	{
-		if (esc(*i - 1))
-			return (0);
-		return (1);
-	}
-	else if (esc(*i))
-		return ((*i = 0));
-	return (!(*i = 0));
-}
-
-int	isQorS(char c)
-{
-	return (c == '\"' || c == '\'' || c == '\\');
+		return (esc(*i));
+	return (!esc(*i) && !(*i = 0));
 }
 
 int	check_start_quotes(char *str, int i, int c1, int *c4)
@@ -235,7 +214,7 @@ int	return_decode(int total, int *x, int i, int flag)
 		return (0);
 	if (x[0] < 0)
 		return (x[0]);
-	if (x[3] != -1)
+	if (x[3] < -1)
 		return (x[3]);
 	if (x[1] == 0 || flag == 0)
 		return (i - total + 1);
@@ -257,7 +236,7 @@ int	count_qs(char *str, int flag)
 			if (flag && check_start_quotes(str, i, x[0], &x[3]))
 				x[3] = (x[3] == -1) ? -3 : -4;
 			if (pross_bksl(str[i], &x[2]) && (total += 1))
-				set_counts(str[i], &x[0], &x[1], &x[2]);
+				set_counts(str[i], &x[0], &x[1]);
 		}
 	}
 	return (return_decode(total, x, i, flag));
