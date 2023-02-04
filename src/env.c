d6440e1b13e5daf0e17c8f919aab37dd05eca8d2
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-02-03 23:59:15 -0800

    Make setenv function correctly for the two arguments and pass all basic tests

--- src/env.c
+++ src/env.c
@@ -75,16 +75,6 @@ int	isQorS(char c)
 	return (c == '\"' || c == '\'' || c == '\\');
 }
 
-int	check_legal_chars(char *str)
-{
-	while (*str)
-		if (*str != '.' && *str != '_' || (*(str++) && 0))
-			if (!ft_isalnum(*str) || (*(str++) && 0))
-				if (!isQorS(*str) || (*(str++) && 0))
-					return(1);
-	return (0);
-}
-
 int	esc(int flag)
 {
 	return (flag % 2);
@@ -126,7 +116,9 @@ char	*prepare_out(char *str, int count)
 	out[count] = '\0';
 	while (str[++i])
 	{
-		if (check_behind(str[i], &b_flag, out[count]) || q_check(out, str[i], count))
+		if (check_behind(str[i], &b_flag, out[count]))
+			out[x++] = str[i];
+		else if (q_check(out, str[i], count))
 			out[x++] = str[i];
 	}
 	out[x] = '\0';
@@ -143,9 +135,9 @@ char	*trim_qs(int count, char *str, int flag)
 	x = 0;
 	if (count == 0)
 		return (str);
-	if (flag * count == -3 && illegal_char_error(&str, 0))
+	if (flag * count == -4 && illegal_char_error(&str, 0))
 		return (NULL);
-	if (flag * count == -4 && start_error(&str, 0))
+	if (flag * count == -3 && start_error(&str, 0))
 		return (NULL);
 	if (count < 0 && quotation_error(count))
 		return (NULL);
@@ -176,33 +168,39 @@ int	pross_bksl(char c, int *i, int c1)
 	return (!esc(*i) * !(*i = 0));
 }
 
-int	wrong_start_check1(char c, int flag)
+int	illegal_c(char c, int flag)
 {
-	if ((c == '\\') * flag)
+	if (c == '\0')
+		return (0);
+	if (c == '\\')
 		return (1);
+	if (c == '.' && flag == 2)
+		return (0);
+	if (ft_isdigit(c) && flag == 2)
+		return (0);
 	if (!ft_isalpha(c) && c != '_' && !isQorS(c))
 		return (1);
 	return (0);
 }
 
-int	check_start_quotes(char *str, int i, int c1, int *c4)
+void	check_start_quotes(char *str, int i, int c1, int *c4)
 {
 	int c[2] = {-1, -2};
+	int c4_status;
 
+	c4_status = (*c4 == 0) ? 1 : 2;
 	if (*c4 == -4 || *c4 == -3)
-		return (0);
-	if (c1 == 0 && wrong_start_check1(str[i], 1))
-		return (1);
+		return ;
+	if (illegal_c(str[i], c4_status))
+		if ((*c4 = *c4 - 3))
+			return;
 	if (c[str[i] % 2] == c1)
-		if (str[i - 1] != str[i])
-			return (1);
-	if (c[str[i] % 2] == c1)
-		if (*c4 == 0 && str[i + 1] == '\0')
-			return (!(*c4 = -4));
-	return (0);
+		if (str[i - 1] != str[i] || illegal_c(str[i + 1], c4_status))
+			if ((*c4 = *c4 - 3))
+				return;
 }
 
-int	set_x(int *alphabet_flag, int c1)
+int	set_x(int *alphabet_flag, int c1, char c)
 {
 	if (*alphabet_flag == 0 && c1 == 0)
 		*alphabet_flag = -1;
@@ -211,12 +209,12 @@ int	set_x(int *alphabet_flag, int c1)
 
 int	return_decode(int total, int *x, int i, int flag)
 {
-	if (total == 0)
-		return (0);
 	if (x[0] < 0)
 		return (x[0]);
 	if (x[3] < -1)
 		return (x[3]);
+	if (total == 0)
+		return (0);
 	return (i - total + 1);
 }
 
@@ -230,13 +228,11 @@ int	count_qs(char *str, int flag)
 	total = 0;
 	while(str[++i])
 	{
-		if (isQorS(str[i]) || set_x(&x[3], x[0]) && (x[2] = 0))
-		{
-			if (flag && check_start_quotes(str, i, x[0], &x[3]))
-				x[3] = (x[3] == -1) ? -3 : -4;
+		if (flag)
+			check_start_quotes(str, i, x[0], &x[3]);
+		if (isQorS(str[i]) || set_x(&x[3], x[0], str[i]) && (x[2] = 0))
 			if (pross_bksl(str[i], &x[2], x[0]) && (total += 1))
 				set_counts(str[i], &x[0], x[2], &total);
-		}
 	}
 	return (return_decode(total, x, i, flag));
 }
@@ -267,12 +263,7 @@ char	**clean_args(char *str, t_hlist **env_h)
 	ar_len = args_len(args);
 	if (ar_len >= 4 && set_env_error(args))
 		return (NULL);
-	if (ar_len > 1 && wrong_start_check1(*(*(args + 1)), 0))
-		if (start_error(args, 1))
-			return (NULL);
 	if (ar_len == 1 && free_args(args, 0) && env(env_h))
-		return (NULL);
-	if (ar_len > 1 && check_legal_chars(args[1]) && illegal_char_error(args, 1))
 		return (NULL);
 	if (ar_len == 2)
 		args = surgery(args);
